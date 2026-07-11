7d5e9e9cd7d7b872df60576ecf72ae2c90ee2e77
Lkkm0124-del <geniusnewzi8@gmail.com>
2026-07-11 12:17:09 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="유튜브 분석기",
+    page_icon="📺",
+    layout="wide"
+)
+
+st.title("📺 유튜브 분석 플랫폼")
+
+st.markdown("""
+### 가능한 기능
+
+✅ 유튜버 검색
+
+✅ 유튜버 영상 조회수 통계
+
+✅ 영상 검색
+
+✅ 댓글 분석
+
+왼쪽 메뉴에서 원하는 기능을 선택하세요.
+""")
